15b28977d8e6751c80af8e9e2e93cca9cb8178c4
Desraj173 <desraj173@gmail.com>
2024-06-18 21:44:35 +0530

    Module Card

--- src/components/Modules/Modules.tsx
+++ src/components/Modules/Modules.tsx
@@ -1,8 +1,25 @@
+import ModuleCard from "./ModuleCard";
+const module = {
+  type: "Quiz",
+  name: "Microsoft Word Proficiency Test",
+  noOfQuestions: 10,
+  skills: ["Document Formatting", "Text Editing", "Template Usage"],
+  time: 15,
+  Weightage: 20,
+};
 
 const Modules = () => {
+  const { name, type, noOfQuestions, skills, time, Weightage } = module;
   return (
-    <div>Modules</div>
-  )
-}
+    <ModuleCard
+      name={name}
+      type={type}
+      noOfQuestions={noOfQuestions}
+      skills={skills}
+      time={time}
+      Weightage={Weightage}
+    />
+  );
+};
 
-export default Modules
+export default Modules;
