4c0bd75e000a4990ae919f511b32a5b9f25a8a50
ajay <ajay.ranga.mr.unique@gmail.com>
2024-06-23 13:47:39 +0530

    form validation on add new question

--- src/components/AddNewQuestion/index.tsx
+++ src/components/AddNewQuestion/index.tsx
@@ -31,11 +31,24 @@ const AddNewQuestion = (props: { questions: any; module?: any; setIsAddNewQuesti
   };
   const [formData, localDispatch] = useReducer(reducer, formInitialState);
 
+  const onSubmitForm = () => {
+    dispatch(
+      addQuestionToModule({
+        name: module.name,
+        question: [...questions, formData],
+      }),
+    );
+    setIsAddNewQuestion(false);
+  };
+
   return (
     <>
       <div className="relative mt-10 mx-10 z-[999]">
         <div className="grid grid-cols-1 h-screen w-full">
-          <div className="flex flex-col w-full h-[50vh] rounded-lg shadow-inner bg-white p-8 overflow-scroll z-50">
+          <form
+            onSubmit={onSubmitForm}
+            className="flex flex-col w-full h-[50vh] rounded-lg shadow-inner bg-white p-8 overflow-scroll z-50"
+          >
             <div className=" w-full">
               <div className="row1  mb-4">
                 <span className="font-bold text-lg">
@@ -43,6 +56,8 @@ const AddNewQuestion = (props: { questions: any; module?: any; setIsAddNewQuesti
                 </span>
               </div>
               <Textarea
+                required
+                minLength={5}
                 rows={6}
                 name="jd_descriptions"
                 id="jd_descriptions"
@@ -57,6 +72,8 @@ const AddNewQuestion = (props: { questions: any; module?: any; setIsAddNewQuesti
                   <input
                     className="py-3 px-3 rounded-lg border border-[#E4A76F] min-w-[25%]"
                     placeholder="Enter Option 1"
+                    required
+                    minLength={1}
                     onChange={(e) =>
                       localDispatch({ type: 'options', payload: e.target.value, index: 0 })
                     }
@@ -67,6 +84,8 @@ const AddNewQuestion = (props: { questions: any; module?: any; setIsAddNewQuesti
                   <input
                     className="py-3 px-3 rounded-lg border border-[#E4A76F] min-w-[25%]"
                     placeholder="Enter Option 2"
+                    required
+                    minLength={1}
                     onChange={(e) =>
                       localDispatch({ type: 'options', payload: e.target.value, index: 1 })
                     }
@@ -77,6 +96,8 @@ const AddNewQuestion = (props: { questions: any; module?: any; setIsAddNewQuesti
                   <input
                     className="py-3 px-3 rounded-lg border border-[#E4A76F] min-w-[25%]"
                     placeholder="Enter Option 3"
+                    required
+                    minLength={1}
                     onChange={(e) =>
                       localDispatch({ type: 'options', payload: e.target.value, index: 2 })
                     }
@@ -87,6 +108,8 @@ const AddNewQuestion = (props: { questions: any; module?: any; setIsAddNewQuesti
                   <input
                     className="py-3 px-3 rounded-lg border border-[#E4A76F] min-w-[25%]"
                     placeholder="Enter Option 4"
+                    required
+                    minLength={1}
                     onChange={(e) =>
                       localDispatch({ type: 'options', payload: e.target.value, index: 3 })
                     }
@@ -97,6 +120,8 @@ const AddNewQuestion = (props: { questions: any; module?: any; setIsAddNewQuesti
                   <input
                     className="py-3 px-3 rounded-lg border border-[#E4A76F] min-w-[25%]"
                     placeholder="Enter correct answer"
+                    required
+                    minLength={1}
                     onChange={(e) =>
                       localDispatch({ type: 'expectedAnswer', payload: e.target.value })
                     }
@@ -104,23 +129,31 @@ const AddNewQuestion = (props: { questions: any; module?: any; setIsAddNewQuesti
                 </div>
               </div>
             </div>
-
-            <div className="flex flex-row mt-6 justify-center">
-              <button
-                onClick={() => {
-                  dispatch(
-                    addQuestionToModule({ name: module.name, question: [...questions, formData] }),
-                    setIsAddNewQuestion(false),
-                  );
-                }}
-                className={
-                  'mt-2 mx-3 items-center justify-center rounded-md border px-6 py-3 text-base font-medium  shadow-sm hover:bg-orange-text focus:outline-none focus:ring-0 active:animate-pulse bg-orange-text text-white'
-                }
-              >
-                Done
-              </button>
+            <div className="flex justify-center">
+              <div className="flex flex-row mt-6 justify-center">
+                <button
+                  onClick={() => {
+                    setIsAddNewQuestion(false);
+                  }}
+                  className={
+                    'mt-2 mx-3 items-center justify-center rounded-md border px-6 py-3 text-base font-medium  shadow-sm hover:bg-orange-text focus:outline-none focus:ring-0 active:animate-pulse bg-orange-text text-white'
+                  }
+                >
+                  Cancel
+                </button>
+              </div>
+              <div className="flex flex-row mt-6 justify-center">
+                <button
+                  type="submit"
+                  className={
+                    'mt-2 mx-3 items-center justify-center rounded-md border px-6 py-3 text-base font-medium  shadow-sm hover:bg-orange-text focus:outline-none focus:ring-0 active:animate-pulse bg-orange-text text-white'
+                  }
+                >
+                  Done
+                </button>
+              </div>
             </div>
-          </div>
+          </form>
         </div>
       </div>
     </>
