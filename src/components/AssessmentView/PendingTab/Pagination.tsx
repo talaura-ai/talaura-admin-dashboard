7bd129d9da2db8910532865176cc8b714fa515df
ajay <ajay.ranga.mr.unique@gmail.com>
2024-07-25 20:38:08 +0530

    fix pagination

--- src/components/AssessmentView/PendingTab/Pagination.tsx
+++ src/components/AssessmentView/PendingTab/Pagination.tsx
@@ -1,6 +1,6 @@
 import { Select } from '@headlessui/react';
 import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/24/outline';
-import { Dispatch, SetStateAction } from 'react';
+import { Dispatch, SetStateAction, useMemo } from 'react';
 
 const Pagination = ({
   totalCandidates,
@@ -17,6 +17,35 @@ const Pagination = ({
   setCurrentPage: Dispatch<SetStateAction<number>>;
   setSizePerPage: Dispatch<SetStateAction<number>>;
 }) => {
+  const paginationTabs = useMemo(() => {
+    if (totalPages <= 6) {
+      return Array.from({ length: totalPages }, (_, i) => i + 1);
+    }
+
+    const paginationTabs = [];
+    const startPages = [1, 2, 3];
+    const endPages = [totalPages - 2, totalPages - 1, totalPages];
+
+    if (currentPage <= 3) {
+      paginationTabs.push(...startPages);
+      paginationTabs.push('...');
+      paginationTabs.push(...endPages);
+    } else if (currentPage >= totalPages - 2) {
+      paginationTabs.push(1);
+      paginationTabs.push('...');
+      paginationTabs.push(...endPages);
+    } else {
+      paginationTabs.push(1);
+      paginationTabs.push('...');
+      paginationTabs.push(currentPage - 1, currentPage, currentPage + 1);
+      paginationTabs.push('...');
+      paginationTabs.push(...endPages);
+    }
+
+    return paginationTabs;
+  }, [currentPage, totalPages]);
+
+  console.log('paginationTabs', paginationTabs);
   return (
     <div className="w-full rounded-b-xl overflow-hidden">
       <div className="bg-white flex justify-between px-4 py-2">
@@ -29,30 +58,28 @@ const Pagination = ({
               <ul className="inline-flex items-center justify-center">
                 <li className="flex justify-center items-center mr-1">
                   <button
-                    onClick={() => setCurrentPage(1)}
+                    onClick={() => setCurrentPage((prev) => prev - 1)}
                     disabled={currentPage === 1}
                     className={`h-6 w-6 text-base leading-tight hover:bg-antiquewhite rounded-md self-end`}
                   >
                     <ChevronLeftIcon className="h-4 w-6" />
                   </button>
                 </li>
-                {Array(totalPages)
-                  .fill('')
-                  .map((_, index) => (
-                    <li key={index} className="flex justify-center items-center mx-1">
-                      <button
-                        onClick={() => setCurrentPage(index + 1)}
-                        className={`h-6 w-6 text-base leading-tight ${
-                          currentPage === index + 1 ? 'bg-antiquewhite' : 'bg-white'
-                        } hover:bg-antiquewhite rounded-md`}
-                      >
-                        {index + 1}
-                      </button>
-                    </li>
-                  ))}
+                {paginationTabs.map((val) => (
+                  <li key={val} className="flex justify-center items-center mx-1">
+                    <button
+                      onClick={() => typeof val === 'number' && setCurrentPage(val)}
+                      className={`h-6 w-6 text-base leading-tight ${
+                        currentPage === val ? 'bg-antiquewhite' : 'bg-white'
+                      } hover:bg-antiquewhite rounded-md`}
+                    >
+                      {val}
+                    </button>
+                  </li>
+                ))}
                 <li className="flex justify-center items-center ml-1">
                   <button
-                    onClick={() => setCurrentPage(totalPages)}
+                    onClick={() => setCurrentPage((prev) => prev + 1)}
                     disabled={currentPage === totalPages}
                     className={`h-6 w-6 text-base leading-tight hover:bg-antiquewhite rounded-md self-end`}
                   >
